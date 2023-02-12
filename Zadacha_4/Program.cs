4e4cde026ca69932e83d7172e65cd48d6c74efa1
elizavetanikolaenko2017 <elizaveta.nikolaenko2017@gmail.com>
2023-02-12 21:01:58 +0300

    Задача №8

--- Zadacha_4/Program.cs
+++ Zadacha_4/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+System.Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите третье число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+if (A > B)
+{
+    System.Console.WriteLine();
+}
